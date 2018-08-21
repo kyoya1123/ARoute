6dd3d6c26d32875a49906819d1fd522a2551219e
Kyoya Yamaguchi <kyoryu32@gmail.com>
2018-08-21 21:54:16 +0900

    hitTestで駅名取得

--- ARoute/Model/RouteEnum.swift
+++ ARoute/Model/RouteEnum.swift
@@ -8,6 +8,8 @@ enum Route {
     case touyoko
     
     static var currentRoute = Route.yamanote
+    static var stations = [String]()
+    static var color = UIColor()
     
     init?(name: String) {
         switch name {
@@ -24,35 +26,37 @@ enum Route {
         default: return nil
         }
         Route.currentRoute = self
+        assignStations()
+        assignColor()
     }
     
-    func color() -> UIColor {
-        switch self {
+    func assignColor() {
+        switch Route.currentRoute {
         case .yamanote:
-            return #colorLiteral(red: 0.6941176471, green: 0.7882352941, blue: 0.2745098039, alpha: 1)
+            Route.color = #colorLiteral(red: 0.6941176471, green: 0.7882352941, blue: 0.2745098039, alpha: 1)
         case .soubu:
-            return #colorLiteral(red: 0.9450980392, green: 0.8078431373, blue: 0.2235294118, alpha: 1)
+            Route.color = #colorLiteral(red: 0.9450980392, green: 0.8078431373, blue: 0.2235294118, alpha: 1)
         case .tyuuou:
-            return #colorLiteral(red: 0.8588235294, green: 0.4117647059, blue: 0.2392156863, alpha: 1)
+            Route.color = #colorLiteral(red: 0.8588235294, green: 0.4117647059, blue: 0.2392156863, alpha: 1)
         case .dennenntoshi:
-            return #colorLiteral(red: 0.09803921569, green: 0.662745098, blue: 0.5529411765, alpha: 1)
+            Route.color = #colorLiteral(red: 0.09803921569, green: 0.662745098, blue: 0.5529411765, alpha: 1)
         case .touyoko:
-            return #colorLiteral(red: 0.8392156863, green: 0.04705882353, blue: 0.2705882353, alpha: 1)
+            Route.color = #colorLiteral(red: 0.8392156863, green: 0.04705882353, blue: 0.2705882353, alpha: 1)
         }
     }
     
-    func stations() -> [String] {
-        switch self {
+    private func assignStations() {
+        switch Route.currentRoute {
         case .yamanote:
-            return ["東京","有楽町","新橋","浜松町","田町","品川","大崎","五反田","目黒","恵比寿","渋谷","原宿","代々木","新宿","新大久保","高田馬場","目白","池袋","大塚","巣鴨","駒込","田端","西日暮里","日暮里","鶯谷","上野","御徒町","秋葉原","神田"]
+            Route.stations = ["東京","有楽町","新橋","浜松町","田町","品川","大崎","五反田","目黒","恵比寿","渋谷","原宿","代々木","新宿","新大久保","高田馬場","目白","池袋","大塚","巣鴨","駒込","田端","西日暮里","日暮里","鶯谷","上野","御徒町","秋葉原","神田"]
         case .soubu:
-            return []
+            Route.stations = ["三鷹","吉祥寺","西荻窪","荻窪","阿佐ヶ谷","高円寺","中野","東中野","大久保","新宿","代々木","千駄ヶ谷","信濃町","四ッ谷","市ヶ谷","飯田橋","水道橋","御茶ノ水","秋葉原","浅草橋","両国","錦糸町","亀戸","平井","新小岩","小岩","市川","本八幡","下総中山","西船橋","船橋","東船橋","津田沼","幕張本郷","幕張","新検見川","稲毛","西千葉","千葉"]
         case .tyuuou:
-            return []
+            Route.stations = []
         case .dennenntoshi:
-            return []
+            Route.stations = []
         case .touyoko:
-            return []
+            Route.stations = []
         }
     }
 }
--- ARoute/ViewController/ViewController.swift
+++ ARoute/ViewController/ViewController.swift
@@ -3,7 +3,7 @@ import ARKit
 import SceneKit
 import CoreLocation
 
-final class ViewController: UIViewController {
+final class ViewController: UIViewController, UIGestureRecognizerDelegate {
     
     @IBOutlet var sceneView: ARSCNView!
     
@@ -12,8 +12,10 @@ final class ViewController: UIViewController {
     
     override func viewDidLoad() {
         setupLocationManager()
-        scrape()
         setupSceneView()
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        tapGesture.delegate = self
+        sceneView.addGestureRecognizer(tapGesture)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -78,9 +80,9 @@ fileprivate extension ViewController {
     func scrape() {
         let doc = Ji(htmlURL: prepareURL())
         let xPaths = ["//*[@id='detail_route_0']/div[1]/div[2]/h3/text()[1]",//出発時刻
-                      "//*[@id='detail_route_0']/div[3]/div[4]/dl[1]/dd/text()",//到着時刻
-                      "//*[@id='detail_route_0']/div[1]/div[2]/dl/dd",//所要時間
-                      "//*[@id='detail_route_0']/div[3]/div[3]/div[2]/ul/li"]//何番線発
+            "//*[@id='detail_route_0']/div[3]/div[4]/dl[1]/dd/text()",//到着時刻
+            "//*[@id='detail_route_0']/div[1]/div[2]/dl/dd",//所要時間
+            "//*[@id='detail_route_0']/div[3]/div[3]/div[2]/ul/li"]//何番線発
         var routeSearchResult = [String]()
         for xPath in xPaths {
             let scrapedText = doc?.xPath(xPath)?.first?.content
@@ -104,13 +106,46 @@ fileprivate extension ViewController {
     }
     
     func sphereNode(color: UIColor) -> SCNNode {
-        let geometry = SCNSphere(radius: 0.02)
+        let geometry = SCNSphere(radius: 0.01)
         geometry.materials.first?.diffuse.contents = color
         return SCNNode(geometry: geometry)
     }
     
-    func putSphere(at pos: SCNVector3, color: UIColor) -> SCNNode {
+    func putSphere(at pos: SCNVector3, color: UIColor, name: String) -> SCNNode {
         let node = sphereNode(color: color)
+        sceneView.scene.rootNode.addChildNode(node)
+        node.position = pos
+        node.name = name
+        return node
+    }
+    
+    func textNode(text: String) -> SCNNode {
+        let geometry = SCNText(string: text, extrusionDepth: 0.01)
+        geometry.alignmentMode = CATextLayerAlignmentMode.center.rawValue
+        if let material = geometry.firstMaterial {
+            material.diffuse.contents = UIColor.white
+            material.isDoubleSided = true
+        }
+        let textNode = SCNNode(geometry: geometry)
+        
+        geometry.font = UIFont.systemFont(ofSize: 0.5)
+        textNode.scale = SCNVector3Make(0.02, 0.02, 0.02)
+        
+        // Translate so that the text node can be seen
+        let (min, max) = geometry.boundingBox
+        textNode.pivot = SCNMatrix4MakeTranslation((max.x - min.x)/2, min.y - 0.5, 0)
+        
+        // Always look at the camera
+        let node = SCNNode()
+        let billboardConstraint = SCNBillboardConstraint()
+        billboardConstraint.freeAxes = SCNBillboardAxis.Y
+        node.constraints = [billboardConstraint]
+        node.addChildNode(textNode)
+        return node
+    }
+    
+    func putText(at pos: SCNVector3, name: String) -> SCNNode {
+        let node = textNode(text: name)
         sceneView.scene.rootNode.addChildNode(node)
         node.position = pos
         return node
@@ -122,16 +157,53 @@ extension ViewController: ARSCNViewDelegate {
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         guard let imageAnchor = anchor as? ARImageAnchor else { return }
         let referenceImage = imageAnchor.referenceImage
-        let route = Route(name: referenceImage.name!)!
+        let _ = Route(name: referenceImage.name!)
         DispatchQueue.global().async {
-            let color = route.color()
-            for i in 0..<route.stations().count {
-                self.putSphere(at: SCNVector3((anchor.transform.columns.3.x - 0.05 * 15) + 0.05 * Float(i), anchor.transform.columns.3.y - 0.01, anchor.transform.columns.3.z - 0.01), color: color)
+            let color = Route.color
+//            var stationsArray = [[String]]()
+            let stationCount = Route.stations.count
+            for i in 0..<stationCount {
+                let sphereNode = self.putSphere(at: SCNVector3((anchor.transform.columns.3.x - 0.04 * 15) + 0.04 * Float(i), anchor.transform.columns.3.y - 0.01, anchor.transform.columns.3.z - 0.01), color: color, name: Route.stations[i])
+            let textNode = self.putText(at: SCNVector3((anchor.transform.columns.3.x - 0.04 * 15) + 0.04 * Float(i), anchor.transform.columns.3.y - 0.01, anchor.transform.columns.3.z + 0.01), name: Route.stations[i])
             }
+//            let index = stationCount / 3
+//            if stationCount % 3 == 0 {
+//                stationsArray.append(Array(Route.stations[0..<index]))
+//                stationsArray.append(Array(Route.stations[index..<index*2]))
+//                stationsArray.append(Array(Route.stations[index*2..<index*3]))
+//            }
+//            for i in 0..<stationsArray[0].count {
+//                let x = anchor.transform.columns.3.x - 0.01 * Float(stationsArray[0].count)
+//                let sphereNode = self.putSphere(at: SCNVector3(x + Float(0.025 * Float(i)), anchor.transform.columns.3.y - 0.01 + 0.15, anchor.transform.columns.3.z - 0.01), color: color)
+//                let textNode = self.putText(at: SCNVector3(x + Float(0.025 * Float(i)), anchor.transform.columns.3.y - 0.01 + 0.15, anchor.transform.columns.3.z + 0.01), name: stationsArray[0][i])
+//            }
+//
+//            for i in 0..<stationsArray[1].count {
+//                let y = (anchor.transform.columns.3.y - 0.01) - 0.008 * Float(stationsArray[1].count)
+//                let sphereNode = self.putSphere(at: SCNVector3(anchor.transform.columns.3.x + 0.03 * Float(stationsArray[0].count / 2), y + Float(0.025 * Float(i)), anchor.transform.columns.3.z - 0.01), color: color)
+//                let textNode = self.putText(at: SCNVector3(anchor.transform.columns.3.x + 0.03 * Float(stationsArray[0].count / 2), y + Float(0.025 * Float(i)), anchor.transform.columns.3.z + 0.01), name: stationsArray[1][i])
+//            }
+//
+//            for i in 0..<stationsArray[2].count {
+//                let x = anchor.transform.columns.3.x - 0.01 * Float(stationsArray[2].count)
+//                let sphereNode = self.putSphere(at: SCNVector3(x + Float(0.025 * Float(i)), anchor.transform.columns.3.y - 0.01 - 0.1, anchor.transform.columns.3.z - 0.01), color: color)
+//                let textNode = self.putText(at: SCNVector3(x + Float(0.025 * Float(i)), anchor.transform.columns.3.y - 0.01 - 0.1, anchor.transform.columns.3.z + 0.01), name: stationsArray[2][i])
+//            }
+        }
+    }
+    
+    @objc func handleTap(_ gestureRecognize: UIGestureRecognizer) {
+        let p = gestureRecognize.location(in: sceneView)
+        
+        let hitResults = sceneView.hitTest(p, options: [:])
+        
+        if hitResults.count != 0 {
+            let result = hitResults[0]
+            let material = result.node
+            print(material.name)
         }
     }
 }
-
 extension ViewController: CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
